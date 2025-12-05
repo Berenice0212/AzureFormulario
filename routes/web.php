9964827e8580ebccd363a78c4ff2cbf221bc42b5
Berenice <139283348+berenicelop04@users.noreply.github.com>
2025-12-04 20:11:07 -0500

    Update web.php
    a

--- routes/web.php
+++ routes/web.php
@@ -3,8 +3,11 @@
 use App\Models\Contacto;
 use Illuminate\Http\Request;
 
-Route::get('/', function () {
-    return view('formulario_contacto');
+//Route::get('/', function () {
+  //  return view('formulario_contacto');
+//});
+Route::get('/prueba', function () {
+    return 'Laravel funciona en Azure!';
 });
 
 Route::post('/save', function (Request $request) {
